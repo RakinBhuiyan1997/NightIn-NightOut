c2710a7684a9b332cd13116f30a878bc466d082a
Rakin <Rakin>
2022-04-24 16:30:10 -0400

    button styling added

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -12,8 +12,8 @@ const HomePage = () => {
 
         <ChoiceContainer>
           <span>Choose The Vibe</span>
-          <span>Night In</span>
-          <span>Night Out</span>
+          <Button>Night In</Button>
+          <Button>Night Out</Button>
         </ChoiceContainer>
       </Container>
     </>
@@ -24,15 +24,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-const TitleContainer = styled.div`
-  flex-direction: column;
-  align-items: center;
-  position: absolute;
-  top: 20%;
-  width: 80%;
-  height: 10%;
-  border: 5px solid black;
+  height: 100vh;
   background-image: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
   background-size: 400% 400%;
   animation: gradient 15s ease infinite;
@@ -48,6 +40,29 @@ const TitleContainer = styled.div`
     }
   }
 `;
+const TitleContainer = styled.div`
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 20%;
+  width: 80%;
+  height: 10%;
+  border: 5px solid black;
+  /* background-image: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  } */
+`;
 const ChoiceContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -57,11 +72,25 @@ const ChoiceContainer = styled.div`
 `;
 
 const NightIn = styled.p`
+  color: white;
   position: relative;
   font-size: 30px;
   margin-bottom: -40px;
   margin-top: 10px;
   margin-left: 5px;
+  -webkit-animation: glow 1s ease-in-out infinite alternate;
+  -moz-animation: glow 1s ease-in-out infinite alternate;
+  animation: glow 1s ease-in-out infinite alternate;
+  @keyframes glow {
+    from {
+      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073,
+        0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+    }
+    to {
+      text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6,
+        0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
+    }
+  }
 `;
 
 const NightOut = styled.p`
@@ -69,6 +98,91 @@ const NightOut = styled.p`
   font-size: 30px;
   text-align: right;
   margin-right: 5px;
+  -webkit-animation: glow 1s ease-in-out infinite alternate;
+  -moz-animation: glow 1s ease-in-out infinite alternate;
+  animation: glow 1s ease-in-out infinite alternate;
+  @keyframes glow {
+    from {
+      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073,
+        0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+    }
+    to {
+      text-shadow: 0 0 20px #fff, 0 0 30px #fd1c03, 0 0 40px #fd1c03,
+        0 0 50px #fd1c03, 0 0 60px #fd1c03, 0 0 70px #fd1c03, 0 0 80px #fd1c03;
+    }
+  }
+`;
+
+const Button = styled.button`
+  width: 220px;
+  height: 30px;
+  border: none;
+  outline: none;
+  margin-top: 20px;
+  color: #fff;
+  background: #111;
+  cursor: pointer;
+  position: relative;
+  z-index: 0;
+  border-radius: 10px;
+  :before {
+    content: "";
+    background: linear-gradient(
+      45deg,
+      #ff0000,
+      #ff7300,
+      #fffb00,
+      #48ff00,
+      #00ffd5,
+      #002bff,
+      #7a00ff,
+      #ff00c8,
+      #ff0000
+    );
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+    border-radius: 10px;
+  }
+  :active {
+    color: #000;
+  }
+  :active:after {
+    background: transparent;
+  }
+  :hover:before {
+    opacity: 1;
+  }
+  :after {
+    z-index: -1;
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: black;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+  }
+  @keyframes glowing {
+    0% {
+      background-position: 0 0;
+    }
+    50% {
+      background-position: 400% 0;
+    }
+    100% {
+      background-position: 0 0;
+    }
+  }
 `;
 
 export default HomePage;
