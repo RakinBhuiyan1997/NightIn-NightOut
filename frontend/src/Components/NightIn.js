a65f11c9ebecec90551652f4d2ee98d65f353d48
Rakin <Rakin>
2022-05-19 15:52:21 -0400

    Selection for option styling complete

--- frontend/src/Components/NightIn.js
+++ frontend/src/Components/NightIn.js
@@ -6,34 +6,73 @@ const NightIn = () => {
   return (
     <Container>
       <StyledLink to="/nightin/drinks">
-        <Drinks>
-          <p>Drinks</p>
-        </Drinks>
+        <Card>
+          <TitleChoice>Drinks</TitleChoice>
+        </Card>
       </StyledLink>
       <StyledLink to="/nightin/games">
-        <Games>
-          <p>Games</p>
-        </Games>
+        <Card>
+          <TitleChoice>Games</TitleChoice>
+        </Card>
       </StyledLink>
       <StyledLink to="/nightin/favorites">
-        <Favorites>
-          <p>Favorites</p>
-        </Favorites>
+        <Card>
+          <TitleChoice>Favorites</TitleChoice>
+        </Card>
       </StyledLink>
       <StyledLink to="/nightin/create">
-        <Create>
-          <p>Create</p>
-        </Create>
+        <Card>
+          <TitleChoice>Create</TitleChoice>
+        </Card>
       </StyledLink>
     </Container>
   );
 };
 
-const Container = styled.div``;
-const Drinks = styled.div``;
-const Games = styled.div``;
-const Favorites = styled.div``;
-const Create = styled.div``;
-const StyledLink = styled(Link)``;
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  text-align: center;
+  margin-top: 100px;
+`;
+const Card = styled.div`
+  width: 125px;
+  height: 125px;
+  border: solid 5px black;
+  margin: 10px;
+  border-radius: 10px;
+  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.7);
+`;
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const TitleChoice = styled.h2`
+  margin-top: 45px;
+  text-transform: uppercase;
+  background-image: linear-gradient(
+    -225deg,
+    #231557 0%,
+    #44107a 29%,
+    #ff1361 67%,
+    #fff800 100%
+  );
+  background-size: auto auto;
+  background-clip: border-box;
+  background-size: 200% auto;
+  color: #fff;
+  background-clip: text;
+  text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: textclip 2s linear infinite;
+  display: inline-block;
+  @keyframes textclip {
+    to {
+      background-position: 200% center;
+    }
+  }
+`;
 
 export default NightIn;
